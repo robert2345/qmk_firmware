418ffc385bfdc48930cc1046acf60c90ce4a72ec
ToxicBob <--user.name>
2018-04-04 19:38:08 +0200

    Added robert keymap for nyquist

---
+++ keyboards/nyquist/keymaps/robert/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define USE_SERIAL
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 150
+
+#endif
---
+++ keyboards/nyquist/keymaps/robert/rules.mk
@@ -0,0 +1,8 @@
+KEY_LOCK_ENABLE = no
+NKRO_ENABLE = no
+CONSOLE_ENABLE = no
+TAP_DANCE_ENABLE = no
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
