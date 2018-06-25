9f0a0b55c5f24c59bf4757fc415f100f4e593ff6
Robert Carlzén <robertc@axis.com>
2018-06-25 15:46:09 +0200

    Reworking to use timer interrupt to toggle solenoid
    Change-Id: I2699bd965336445495c00ab56d43207137fa40bd

--- keyboards/nyquist/keymaps/robert/keymap.c
+++ keyboards/nyquist/keymaps/robert/keymap.c
@@ -3,11 +3,34 @@
 #include "action_util.h"
 #include "eeconfig.h"
 #include "quantum_keycodes.h"
+#include "pincontrol.h"
 
 #define _______ KC_TRNS
 
+#define SOLENOID_DEFAULT_DWELL 75 
+#define SOLENOID_MAX_DWELL 100
+#define SOLENOID_MIN_DWELL 4
+#define SOLENOID_PIN D3
+
+#define TIMER2 2
+
 extern keymap_config_t keymap_config;
 
+enum planck_keycodes {
+  SOLENOID_TOG = SAFE_RANGE,
+  SOLENOID_DWELL_MINUS,
+  SOLENOID_DWELL_PLUS,
+  SOLENOID_BUZZ_ON,
+  SOLENOID_BUZZ_OFF,
+  EXT_PLV
+};
+
+bool solenoid_enabled = false;
+bool solenoid_on = false;
+bool solenoid_buzz = false;
+bool solenoid_buzzing = false;
+uint16_t solenoid_start = 0;
+uint8_t solenoid_dwell = SOLENOID_DEFAULT_DWELL;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -33,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______),
 
     KEYMAP(
-            RESET,    _______,  _______,  _______,  _______,  _______,  _______/*FC_TOG*/,         _______,  KC_SLSH,  KC_PAST,  KC_PMNS,        KC_DEL,
+            RESET,    _______,  _______,  _______,  _______,  _______,  SOLENOID_TOG,         _______,  KC_SLSH,  KC_PAST,  KC_PMNS,        KC_DEL,
             _______,  _______,  _______,  _______,  _______,  _______,  _______/*CLICKY_TOGGLE*/,  KC_7,     KC_8,     KC_9,     KC_PLUS,        _______,
             _______,  _______,  _______,  _______,  _______,  _______,  AU_TOG,         KC_4,     KC_5,     KC_6,     LSFT(KC_SCLN),  KC_EQUAL,
             _______,  _______,  _______,  _______,  _______,  _______,  MU_TOG,         KC_1,     KC_2,     KC_3,     KC_ENT,         _______,
@@ -45,13 +68,168 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   return MACRO_NONE;
 }
 
+void solenoid_buzz_on(void) {
+  solenoid_buzz = true;
+}
+
+void solenoid_buzz_off(void) {
+  solenoid_buzz = false;
+}
+
+void solenoid_dwell_minus(void) {
+  if (solenoid_dwell > 0) solenoid_dwell--;
+}
+
+void solenoid_dwell_plus(void) {
+  if (solenoid_dwell < SOLENOID_MAX_DWELL) solenoid_dwell++;
+}
+
+void solenoid_toggle(void) {
+  solenoid_enabled = !solenoid_enabled;
+} 
+
+void solenoid_stop(void) {
+  digitalWrite(SOLENOID_PIN, PinLevelLow);
+  solenoid_on = false;
+  //solenoid_buzzing = false;
+}
+
+void solenoid_check(void) {
+  uint16_t elapsed = 0;
+
+  if (!solenoid_on) return;
+
+  elapsed = timer_elapsed(solenoid_start);
+
+  //Check if it's time to finish this solenoid click cycle 
+  if (elapsed > solenoid_dwell) {
+    solenoid_stop();
+    return;
+  }
+
+  //Check whether to buzz the solenoid on and off
+  if (solenoid_buzz) {
+    if (elapsed / SOLENOID_MIN_DWELL % 2 == 0){
+      if (!solenoid_buzzing) {
+        solenoid_buzzing = true;
+        digitalWrite(SOLENOID_PIN, PinLevelHigh);
+      }
+    }
+    else {
+      if (solenoid_buzzing) {
+        solenoid_buzzing = false;
+        digitalWrite(SOLENOID_PIN, PinLevelLow);
+      }
+    }
+  }
+  
+}
+
+void solenoid_fire(void) {
+    /*
+  if (!solenoid_enabled) return;
+
+  if (!solenoid_buzz && solenoid_on) {
+     solenoid_check();
+     return;
+  }
+
+  if (solenoid_buzz && solenoid_buzzing) return;
+*/
+
+//timer variant
+if (solenoid_on) return;
+  solenoid_on = true;
+  //solenoid_buzzing = true;
+  enable_timer();
+  solenoid_start = timer_read(); 
+  digitalWrite(SOLENOID_PIN, PinLevelHigh);
+}
+
+void solenoid_setup(void) {
+  pinMode(SOLENOID_PIN, PinDirectionOutput);
+  setup_timer();
+}
+
 void matrix_init_user(void) {
+  solenoid_setup();
 }
 
 void matrix_scan_user(void) {
+  //solenoid_check();
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+ 
+  if (record->event.pressed) {
+    solenoid_fire(); 
+  }
+
+  switch (keycode) {
+    /*case SING_RANDOM_SONG:
+      if (record->event.pressed) {
+        sing_random_song();
+      }
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case BACKLIT:
+      if (record->event.pressed) {
+        register_code(KC_RSFT);
+        #ifdef BACKLIGHT_ENABLE
+          backlight_step();
+        #endif
+        PORTE &= ~(1<<6);
+      } else {
+        unregister_code(KC_RSFT);
+        PORTE |= (1<<6);
+      }
+      return false;
+      break;*/
+    case SOLENOID_TOG:
+      if (record->event.pressed) {
+        solenoid_toggle();
+      }
+      break;
+    case SOLENOID_DWELL_MINUS:
+      if (record->event.pressed) {
+        solenoid_dwell_minus();
+      }
+      break;
+    case SOLENOID_DWELL_PLUS:
+      if (record->event.pressed) {
+        solenoid_dwell_plus();
+      }
+      break;
+    case SOLENOID_BUZZ_ON:
+      if (record->event.pressed) {
+        solenoid_buzz_on();
+      }
+      break;
+    case SOLENOID_BUZZ_OFF:
+      if (record->event.pressed) {
+        solenoid_buzz_off();
+      }
+      break;
+  }
   return true;
 }
 
@@ -88,4 +266,89 @@ void led_set_user(uint8_t usb_led) {
   }
 
 }
+
+setup_timer() {
+    // 8 bit timer
+    TCCR2A = 0;
+    TCCR2B = 0;
+    bitWrite(TCCR2A, WGM21, 1);
+    bitWrite(TCCR2B, CS20, 1);
+    timer2_pin_port = portOutputRegister(digitalPinToPort(SOLENOID_PIN));
+    timer2_pin_mask = digitalPinToBitMask(SOLENOID_PIN);
+}
+
+void enable_timer()
+{
+  uint8_t prescalarbits = 0b001;
+  long toggle_count = 0;
+  uint32_t ocr = 0;
+  int8_t _timer;
+
+  frequency = 20
+
+  ocr = F_CPU / frequency / 2 - 1;
+  prescalarbits = 0b001;  // ck/1: same for both timers
+  if (ocr > 255)
+  {
+      ocr = F_CPU / frequency / 2 / 8 - 1;
+      prescalarbits = 0b010;  // ck/8: same for both timers
+
+      if (_timer == 2 && ocr > 255)
+      {
+          ocr = F_CPU / frequency / 2 / 32 - 1;
+          prescalarbits = 0b011;
+      }
+
+      if (ocr > 255)
+      {
+          ocr = F_CPU / frequency / 2 / 64 - 1;
+          prescalarbits = _timer == 0 ? 0b011 : 0b100;
+
+          if (_timer == 2 && ocr > 255)
+          {
+              ocr = F_CPU / frequency / 2 / 128 - 1;
+              prescalarbits = 0b101;
+          }
+
+          if (ocr > 255)
+          {
+              ocr = F_CPU / frequency / 2 / 256 - 1;
+              prescalarbits = _timer == 0 ? 0b100 : 0b110;
+              if (ocr > 255)
+              {
+                  // can't do any better than /1024
+                  ocr = F_CPU / frequency / 2 / 1024 - 1;
+                  prescalarbits = _timer == 0 ? 0b101 : 0b111;
+              }
+          }
+      }
+  }
+
+  TCCR2B = prescalarbits;
+
+  // Set the OCR for the given timer,
+  // set the toggle count,
+  // then turn on the interrupts
+#if defined(OCR2A) && defined(TIMSK2) && defined(OCIE2A)
+  OCR2A = ocr;
+  bitWrite(TIMSK2, OCIE2A, 1);
+#endif
+
+}
+
+
+void disableTimer(uint8_t _timer)
+{
+    bitWrite(TIMSK2, OCIE2A, 0); // disable interrupt
+    TCCR2A = (1 << WGM20);
+    TCCR2B = (TCCR2B & 0b11111000) | (1 << CS22);
+    OCR2A = 0;
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+ solenoid_stop();
+ disableTimer();
+}
+
 
